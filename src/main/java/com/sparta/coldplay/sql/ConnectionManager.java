da6c86ee3ff7a78c70ca08c39ba095e7075db569
Aleksander Napieralski <aleksander.napieralski@city.ac.uk>
2022-08-17 15:26:57 +0100

    Connection Manager restored

--- src/main/java/com/sparta/coldplay/sql/ConnectionManager.java
+++ src/main/java/com/sparta/coldplay/sql/ConnectionManager.java
@@ -1,4 +1,32 @@
 package com.sparta.coldplay.sql;
+import com.sparta.coldplay.utils.PropertiesLoader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionManager {
 
+    private static Connection postgresCon;
+    public static Connection connectionToDB(){
+        String url = PropertiesLoader.getProperty("url");
+        String userName = PropertiesLoader.getProperty("userName");
+        String password = PropertiesLoader.getProperty("password");
+        try {
+            postgresCon = DriverManager.getConnection(url, userName, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return postgresCon;
+    }
+
+    public static void closeConnection(){
+        try {
+            postgresCon.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
+
+
