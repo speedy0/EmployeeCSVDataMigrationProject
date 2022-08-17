a483251f111934cb0a9e285a6e671c36c619cf40
Aleksander Napieralski <aleksander.napieralski@city.ac.uk>
2022-08-17 15:31:12 +0100

    SQL Queries have been designed

--- src/main/java/com/sparta/coldplay/sql/SQLQueries.java
+++ src/main/java/com/sparta/coldplay/sql/SQLQueries.java
@@ -1,4 +1,7 @@
 package com.sparta.coldplay.sql;
+
 public interface SQLQueries {
+    public static final String SELECT_ALL = "SELECT * FROM public.employee_db";
+    public static final String INSERT_INTO_DB = "INSERT INTO public.employee_db (id, name_prefix, first_name, middle_initial, last_name, gender, email, date_of_birth, date_of_joining, salary) VALUES (?,?,?,?,?,?,?,?,?,?)";
 
 }
