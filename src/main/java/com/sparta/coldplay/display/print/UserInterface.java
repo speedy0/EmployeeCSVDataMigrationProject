2e8d1d4a3a058c4ae281e8d347b4a2e8bdc41f7f
Aleksander Napieralski <aleksander.napieralski@city.ac.uk>
2022-08-18 16:01:10 +0100

    Implementation of the UserInterface

---
+++ src/main/java/com/sparta/coldplay/display/print/UserInterface.java
@@ -0,0 +1,24 @@
+package com.sparta.coldplay.display.print;
+
+import com.sparta.coldplay.dao.EmployeeDAO;
+
+public class UserInterface {
+    public static void printCleanEmployees(){
+        System.out.println("The Clean employee records : ");
+        for (int i=0; i< EmployeeDAO.getEmployees().size(); i++){
+            System.out.println(EmployeeDAO.getEmployees().get(i));
+        }
+    }
+    public static void printDuplicatedEmployees(){
+        System.out.println("The Duplicated employee records : ");
+        for (int i=0; i< EmployeeDAO.getDuplicatedEmployees().size(); i++){
+            System.out.println(EmployeeDAO.getDuplicatedEmployees().get(i));
+        }
+    }
+    public static void printCorruptedEmployees(){
+        System.out.println("The Corrupted employee records : ");
+        for (int i=0; i< EmployeeDAO.getCorruptedEmployees().size(); i++){
+            System.out.println(EmployeeDAO.getCorruptedEmployees().get(i));
+        }
+    }
+}
