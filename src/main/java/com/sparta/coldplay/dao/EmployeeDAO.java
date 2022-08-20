8789b3cb4212e378ac1ab58e113bb197daccb9be
Aleksander Napieralski <aleksander.napieralski@city.ac.uk>
2022-08-20 11:34:38 +0100

    Added Logger into the system

--- src/main/java/com/sparta/coldplay/dao/EmployeeDAO.java
+++ src/main/java/com/sparta/coldplay/dao/EmployeeDAO.java
@@ -1,6 +1,7 @@
 package com.sparta.coldplay.dao;
 
 import com.sparta.coldplay.dto.EmployeeDTO;
+import com.sparta.coldplay.logger.LoggerSystem;
 import com.sparta.coldplay.sql.SQLQueries;
 
 import java.sql.*;
@@ -19,14 +20,17 @@ public class EmployeeDAO {
     public EmployeeDAO(Connection postgresConn){
         this.postgresConn = postgresConn;
         try {
+            LoggerSystem.getMessage(1, "Creating a statement and connection using EmployeeDAO class.");
             statement = postgresConn.createStatement();
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
+            LoggerSystem.getMessage(2, e.getMessage());
         }
     }
 
     public void printAllEmployees(){
         try {
+            LoggerSystem.getMessage(1, "Printing all of the employees using SELECT query.");
             ResultSet resultSet = statement.executeQuery(SQLQueries.SELECT_ALL);
             while (resultSet.next()){
                 System.out.println(resultSet.getString(1));
@@ -42,43 +46,52 @@ public class EmployeeDAO {
             }
         } catch (SQLException e) {
             e.printStackTrace();
+            LoggerSystem.getMessage(2, e.getMessage());
         }
     }
 
     public void createTable()  {
         deleteTable();
         try {
+            LoggerSystem.getMessage(1, "Creating a table using Create Table query.");
             PreparedStatement preparedStatement = postgresConn.prepareStatement(SQLQueries.CREATE_TABLE);
             preparedStatement.execute();
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
+            LoggerSystem.getMessage(2, e.getMessage());
         }
     }
 
     public void deleteTable(){
         PreparedStatement preparedStatement = null;
         try {
+            LoggerSystem.getMessage(1, "Dropping the table if exists.");
             preparedStatement = postgresConn.prepareStatement(SQLQueries.DROP_TABLE);
             preparedStatement.execute();
         }
         catch (SQLException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
+            LoggerSystem.getMessage(2, e.getMessage());
         }
     }
 
     public void truncateTable(){
         PreparedStatement preparedStatement = null;
         try {
+            LoggerSystem.getMessage(1, "Trying to truncate the table.");
             preparedStatement = postgresConn.prepareStatement(SQLQueries.TRUNCATE_TABLE);
             preparedStatement.execute();
+            LoggerSystem.getMessage(1, "Query has been sent out correctly for truncating the table.");
         }
         catch (SQLException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
+            LoggerSystem.getMessage(2, e.getMessage());
         }
     }
 
     public void createEmployee(String id, String name_prefix, String first_name, String middle_initial, String last_name, String gender,
                                String email, LocalDate date_of_birth, LocalDate date_of_joining, int salary){
+        LoggerSystem.getMessage(1, "Creating an employee in EmployeeDAO class.");
         try {
             PreparedStatement preparedStatement = postgresConn.prepareStatement(SQLQueries.INSERT_INTO_DB);
             preparedStatement.setString(1, id);
@@ -92,8 +105,10 @@ public class EmployeeDAO {
             preparedStatement.setDate(9, Date.valueOf(date_of_joining));
             preparedStatement.setInt(10, salary);
             preparedStatement.execute();
+            LoggerSystem.getMessage(1, "Employee created and added into the database.");
         } catch (SQLException e) {
             e.printStackTrace();
+            LoggerSystem.getMessage(2, e.getMessage());
         }
     }
 }
